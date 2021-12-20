3829012ae076c2117b8b3bc38b6d0f3285caeb7c
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2021-12-20 18:30:16 -0500

    Add files via upload

---
+++ typofart.cs
@@ -0,0 +1,47 @@
+var fart = true
+using fart
+
+main void()fart
+
+namespace()fart
+
+void update()where fart?"  let fart = "GONE."
+
+void update() var fart  = false 
+var gone = true 
+
+
+void update() var fart = true var gone = false let fart = monke
+
+var fart = false
+var monke = true  
+
+void update() main void() monke 
+
+namespace()monke /* Update */
+ 
+ new() var = monke 
+ 
+var funni = true 
+
+public class()funni 
+
+var funni = false 
+/* funni is gone now. updates to the class. */
+
+public class()false
+ /* false = none */ 
+
+ var main = true
+ 
+public main() main 
+
+/* branch: main */ 
+
+var GITHUB = true 
+
+private void() GITHUB 
+
+
+ 
+ 
